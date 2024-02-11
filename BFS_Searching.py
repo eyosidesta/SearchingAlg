3af8200252ca6bbb2cf7e797b0757cdb8edd58ab
eyosidesta <mamesmsdesta@gmail.com>
2024-02-11 02:53:13 -0500

    store data method defined

--- BFS_Searching.py
+++ BFS_Searching.py
@@ -6,6 +6,33 @@ class BFS_Search:
     def __int__(self):
         pass
 
+    def store_data(self, graph, start, goal):
+        store_graph = []
+        visited_graph = []
+        for key in graph.keys():
+            if key == start:
+                if(self.is_goal(start, goal)):
+                    return start, visited_graph, store_graph
+                store_graph = self.enqueue(store_graph, start)
+                
+                break
+            
+        
+
+
+
+    def is_visited(self, arr, value):
+        for i in range(len(arr)):
+            if(arr[i] == value):
+                return True
+            
+        return False
+    
+    def is_goal(self, value, goal):
+        if(value == goal):
+            return True
+        return False
+
     def enqueue(self, arr, new_val):
         new_arr = [None] * (len(arr) + 1)
         for i in range(0, len(arr)):
@@ -35,5 +62,14 @@ dequeued = bfs.dequeue(array_val)
 enqueued = bfs.enqueue(array_val, "E")
 print("the dequeued value is: ", dequeued)
 print("the enqueued value is: ", enqueued)
+
+maze = {"A": ["D", "B"], "B": ["A", "C"], "C": ["B", "D", "H", "G"], "D": ["A", "E", "F", "C"], 
+        "E": ["D", "I"], "F": ["D", "J"], "G": ["C"], "H": ["C", "L"], "I": ["E", "M", "K", "J"], 
+        "J": ["F", "I", "N", "L"], "K": ["I", "N"], "L": ["J", "H", "N"], "N": ["O"], "O": ["N"]
+        }
+
+print("the maze is: ", maze.get("A"))
+for key in maze.keys():
+    print("the keys are: ", key)
 
 
