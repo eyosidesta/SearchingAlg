f5ac3c96699977212ef71eb3c227f14a2f4ea5f4
eyosidesta <mamesmsdesta@gmail.com>
2024-02-11 02:21:29 -0500

    deque function added

--- BFS_Searching.py
+++ BFS_Searching.py
@@ -6,12 +6,30 @@ class BFS_Search:
     def __int__(self):
         pass
 
-    def enqueue(self, ):
+    def enqueue(self, arr):
         pass
 
-    def dequeue(self, ):
-        pass
+    def dequeue(self, arr):
+        if(self.isEmpty(arr)):
+            return arr
+        # Object new_arr = new object[]
 
+        new_arr = [None] * (len(arr) - 1)
+        print("the len: ", len(new_arr))
+        for i in range(1, len(arr)):
+            new_arr[i-1] = arr[i]
+        return new_arr
+
+    def isEmpty(self, arr):
+        if(len(arr) == 0):
+            return True
+        return False
     
+
+array_val = ["A", "B", "C", "D"]
+
+bfs = BFS_Search()
+dequeued = bfs.dequeue(array_val)
+print("the dequeued value is: ", dequeued)
 
 
