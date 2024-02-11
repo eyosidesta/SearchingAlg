58d641d426f83fda48d2ed45ff181743b0cab674
eyosidesta <mamesmsdesta@gmail.com>
2024-02-11 02:09:46 -0500

    start enqueue, and dequeue methods

--- BFS_Searching.py
+++ BFS_Searching.py
@@ -1,3 +1,17 @@
+maze = {"A": ["D", "B"], "B": ["A", "C"], "C": ["B", "D", "H", "G"], "D": ["A", "E", "F", "C"], 
+        "E": ["D", "I"], "F": ["D", "J"], "G": ["C"], "H": ["C", "L"], "I": ["E", "M", "K", "J"], 
+        "J": ["F", "I", "N", "L"], "K": ["I", "N"], "L": ["J", "H", "N"], "N": ["O"], "O": ["N"]
+        }
 class BFS_Search:
     def __int__(self):
         pass
+
+    def enqueue(self, ):
+        pass
+
+    def dequeue(self, ):
+        pass
+
+    
+
+
